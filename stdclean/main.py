412bad9506cf2218e2f8cdc029c61efd33f360ba
Ian Dinwoodie <dinwoodieian@gmail.com>
2021-02-16 18:23:54 -0500

    Call the patch module from the main loop.

--- stdclean/main.py
+++ stdclean/main.py
@@ -1,8 +1,10 @@
 """Main entry point for the stdclean command."""
 
 from stdclean.find import find_cpp_file_paths
+from stdclean.patch import add_using_declarations
 
 
 def stdclean(target):
     path_list = find_cpp_file_paths(target)
-    [print(p) for p in path_list]
+    for path in path_list:
+        add_using_declarations(path)
